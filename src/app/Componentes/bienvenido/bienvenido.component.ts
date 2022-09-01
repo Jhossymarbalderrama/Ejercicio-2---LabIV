f406e68489b2c48bd1285eaf231d47631de63013
unknown <jhossymarbalderrama@gmail.com>
2022-09-01 14:14:26 -0300

    Ejercicio 2

--- src/app/Componentes/bienvenido/bienvenido.component.ts
+++ src/app/Componentes/bienvenido/bienvenido.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-bienvenido',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BienvenidoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
 
+  volver():void{
+    this.router.navigateByUrl('/login');
+  }
 }
--- src/app/Componentes/login/login.component.ts
+++ src/app/Componentes/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/Entidades/usuario';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -14,10 +16,34 @@ export class LoginComponent implements OnInit {
   //   el componente "error"
   //   ( dejar los componentes listos)
   //   opcional: hacer ruteo
+  nombre: string = "";
+  clave: string = "";
   
-  constructor() { }
+
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
 
+  login():void{
+    let campoVacio: boolean = false;
+
+    this.nombre; 
+    this.clave;
+
+    if(this.nombre == null || this.nombre == undefined || this.nombre == ""){
+      campoVacio = true;      
+    }
+
+    if(this.nombre == null || this.nombre == undefined || this.nombre == ""){
+      campoVacio = true;
+    }
+    
+    if(campoVacio){
+      this.router.navigateByUrl('/error');
+    }else{
+      this.router.navigateByUrl('/bienvenido');
+    } 
+ 
+  }
 }
